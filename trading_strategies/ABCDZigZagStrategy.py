a6c5ca28aa7ea8007749c04ed9361b2c40c2ccda
Patrick <pmcl2472@uni.sydney.edu.au>
2022-10-08 17:01:24 +1100

    ABCD fixups

--- trading_strategies/ABCDZigZagStrategy.py
+++ trading_strategies/ABCDZigZagStrategy.py
@@ -13,12 +13,13 @@ class ZigZagABCD:
         self.ZigZagObject = ZigZagWriter()
         self.ABCDThreshMean = ABCDThreshMean
         self.ABCDThreshVar = ABCDThreshVar
+        self.Name = "ZigZagABCD"
 
     def ZigZagPrep(self, prelimData, startDate):
         self.ZigZagObject.readPrelimData(prelimData, startDate)
 
     def addIndicators(self, data):
-        self.indicatorDF = self.ZigZagDat[['time', 'ZigZag Value', 'ZigZag Type']].iloc[-len(data):].reset_index(drop = True)
+        self.indicatorDF = self.ZigZagDat[['time', 'LowMapBuffer', 'HighMapBuffer', 'ZigZag Value', 'ZigZag Type']].iloc[-len(data):].reset_index(drop = True)
 
     def run(self, data):        
         self.ZigZagDat = self.ZigZagObject.run(data, 1)
